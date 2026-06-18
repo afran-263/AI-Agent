3dbc7a0905c896518b0a7d95adfe016aa7736628
Muhammad Afran <mhdafran.263@gmail.com>
2026-06-18 10:46:05 +0530

    Add files via upload

---
+++ Day7/HRMS Agent Local/Utils/OpenApiExporter.cs
@@ -0,0 +1,27 @@
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi;
+
+namespace HrmsAgentApi;
+
+public static class OpenApiExporter
+{
+    public static async Task ExportAsync(
+        IServiceProvider services,
+        string outputFile)
+    {
+        using var scope = services.CreateScope();
+
+        var swaggerProvider =
+            scope.ServiceProvider.GetRequiredService<ISwaggerProvider>();
+
+        var swaggerDocument =
+            swaggerProvider.GetSwagger("v1");
+
+        await using var stream = File.Create(outputFile);
+
+        await swaggerDocument.SerializeAsJsonAsync(
+            stream,
+            OpenApiSpecVersion.OpenApi3_0,
+            CancellationToken.None);
+    }
+}
